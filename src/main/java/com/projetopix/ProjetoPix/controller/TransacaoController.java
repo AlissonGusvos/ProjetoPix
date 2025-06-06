52044ea0e039ecfab7ca218b7f5e1e68ac0ce494
AlissonGusvos <gustavoalisson322@gmail.com>
2025-06-06 12:43:57 -0300

    Funcao de transferencia adicionada

--- src/main/java/com/projetopix/ProjetoPix/controller/TransacaoController.java
+++ src/main/java/com/projetopix/ProjetoPix/controller/TransacaoController.java
@@ -1,13 +1,28 @@
 package com.projetopix.ProjetoPix.controller;
 
+import com.projetopix.ProjetoPix.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TransacaoController {
+
+    @Autowired
+    private TransacaoService transacaoService;
+
     @GetMapping("/pix")
     public String pix(Model model){
         return "pix";
+    }
+
+    @PostMapping("/pix")
+    public String transacao(@RequestParam String chave, @RequestParam Double valor){
+        transacaoService.transferir(chave, valor);
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/projetopix/ProjetoPix/service/TransacaoService.java
+++ src/main/java/com/projetopix/ProjetoPix/service/TransacaoService.java
@@ -10,10 +10,25 @@ public class TransacaoService {
 
     @Autowired
     private ContaRepository contaRepository;
+    @Autowired
+    private ContaService contaService;
 
     public Conta validarChave(String chave){
         return contaRepository.findByCpf(chave)
                 .orElseThrow(() -> new RuntimeException("Conta não encontrada"));
+    }
+
+    public boolean transferir(String chave, Double valor){
+        Conta mainUser = contaService.mainUser();
+        Conta contaDestino = validarChave(chave);
+
+        contaDestino.setSaldo(contaDestino.getSaldo() + valor);
+        contaRepository.save(contaDestino);
+
+        mainUser.setSaldo(mainUser.getSaldo() - valor);
+        contaRepository.save(mainUser);
+
+        return true;
     }
 
 
